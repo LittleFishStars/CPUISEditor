e507c7862ebf7fc7e7b90f4aafdac9a24847a7ee
月落星沉 <ylxc_email@yeah.net>
2025-08-02 15:08:41 +0800

    v1.2.0
    新增功能：
    1. 添加了缩放的功能
    2. 所有设置项均不会随软件的关闭而复原
    优化了软件操作
    修复了已知的bug

--- resources/scripts/main.gd
+++ resources/scripts/main.gd
@@ -11,6 +11,7 @@ var file: Dictionary:
 		return $ScrollContainer/Edit.file
 	set(new_file):
 		$ScrollContainer/Edit.file = new_file
+
 var path: String = 'user://temp.cis':
 	set(new_path):
 		path = new_path
@@ -22,28 +23,34 @@ var path: String = 'user://temp.cis':
 		if new_path == 'user://temp.cis':
 			file_name = '新文件'
 		DisplayServer.window_set_title('指令集编辑器--' + file_name)
+
 var setting: Dictionary = {
+	'version': 2,
 	'last': self.path,
-	'last_open_path': 'user://'
+	'size': 64,
+	'scale': 1
 }
 
 func _ready() -> void:
 	DisplayServer.window_set_title('指令集编辑器--新文件')
-	self.get_setting()
-	self.path = self.setting.last
-
-func get_setting() -> void:
+	
 	var setting = JsonReader.read('user://config.json')
 	if setting.state:
 		if setting.state == JsonReader.state.READ_ERROR:
 			if setting.code == ERR_FILE_NOT_FOUND:
 				JsonReader.write('user://config.json', self.setting)
 	else:
-		self.setting = setting.data
+		if setting.data.has('version') and (setting.data.version >= self.setting.version):
+			self.setting = setting.data
+			$ScrollContainer/Edit.button_size = setting.data['size']
+			get_tree().root.content_scale_factor = setting.data['scale']
+
+	self.path = self.setting.last
 
 func _notification(what):
 	if (what == NOTIFICATION_WM_CLOSE_REQUEST) or (what == NOTIFICATION_APPLICATION_PAUSED):
 		self.save(self.path)
+		self.setting.last == self.path
 		JsonReader.write('user://config.json', self.setting)
 		get_tree().quit()
 
@@ -58,7 +65,7 @@ func _on_sort_children() -> void:
 func _on_file_id_pressed(id: int) -> void:
 	match id:
 		1:
-			$OpenFile.current_path = self.setting.last_open_path
+			$OpenFile.current_path = self.path
 			$OpenFile.show()
 		2:
 			self.path = 'user://temp.cis'
@@ -73,6 +80,9 @@ func _on_file_id_pressed(id: int) -> void:
 		5:
 			$ScrollContainer/Edit.clean()
 		6:
+			$Setting.setting['bit'] = self.file.bit
+			$Setting.setting['size'] = $ScrollContainer/Edit.button_size
+			$Setting.setting['scale'] = get_tree().root.content_scale_factor
 			$Setting.show()
 
 func show_error(massage: String) -> void:
@@ -89,13 +99,12 @@ func read(path: String) -> Dictionary:
 				self.show_error('文件解析错误！\n错误信息：' + json.massage + '，行数：' + str(json.line))
 			_:
 				self.show_error('发生错误！')
-		return {}
+		return self.new_file
 	return json.data
 
 func _on_open_file_file_selected(path: String) -> void:
 	self.path = path
-	self.file = self.read(path)
-	self.setting.last_open_path = path
+	self.setting.last = path
 
 func save(path: String) -> void:
 	var state = JsonReader.write(path, self.file)
@@ -103,6 +112,7 @@ func save(path: String) -> void:
 		JsonReader.state.OK:
 			$Massage.dialog_text = '保存成功！'
 			$Massage.show()
+			self.path = path
 		JsonReader.state.WRITE_ERROR:
 			self.show_error('文件读取错误！\n错误代码：' + str(state.code))
 		_:
@@ -111,6 +121,9 @@ func save(path: String) -> void:
 func _on_save_file_file_selected(path: String) -> void:
 	self.save(path)
 
-func _on_setting_close(bit: int, button_size: int) -> void:
+func _on_setting_close(bit: int, button_size: int, scale: float) -> void:
 	$ScrollContainer/Edit.num = bit
 	$ScrollContainer/Edit.button_size = button_size
+	get_tree().root.content_scale_factor = scale
+	self.setting['size'] = button_size
+	self.setting['scale'] = scale
--- resources/scripts/setting.gd
+++ resources/scripts/setting.gd
@@ -1,10 +1,23 @@
 extends Window
 
-signal close(bit: int, button_size: int)
+signal close(bit: int, button_size: int, scale: float)
+
+var setting: Dictionary = {
+	'bit': 8, 
+	'size': 64,
+	'scale': 1
+}
 
 func _on_close_requested() -> void:
 	self.close.emit(
 		$ScrollContainer/SettingList/Bit/BitSet.value, 
-		$ScrollContainer/SettingList/Size/SizeSet.value
+		$ScrollContainer/SettingList/Size/SizeSet.value,
+		$ScrollContainer/SettingList/Scale/ScaleSet.value
 	)
 	self.hide()
+
+func _on_visibility_changed() -> void:
+	if self.visible:
+		$ScrollContainer/SettingList/Bit/BitSet.value = self.setting['bit']
+		$ScrollContainer/SettingList/Size/SizeSet.value = self.setting['size']
+		$ScrollContainer/SettingList/Scale/ScaleSet.value = self.setting['scale']
